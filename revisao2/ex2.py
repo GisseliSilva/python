c6e2b23cf8fd38e224394eeb7d0877c12b64a2dc
GisseliSilva <gisseli.rocha@icloud.com>
2025-08-14 14:11:41 -0300

    segundo commit

---
+++ revisao2/ex2.py
@@ -0,0 +1,3 @@
+'''2.	Faça um programa que receba a altura e o sexo de uma pessoa 
+e calcule e mostre seu peso ideal, utilizando as seguintes fórmulas 
+(onde h corresponde à altura): - Homens: (72,7 x h) - 58 - Mulheres: (62,1 x h) - 44,7'''
