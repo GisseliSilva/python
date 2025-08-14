e291d821f497df3bf9a3d7f0a8baabbcdc83fa1f
GisseliSilva <gisseli.rocha@icloud.com>
2025-08-14 16:43:31 -0300

    segundo commit

--- revisao2/ex10.py
+++ revisao2/ex10.py
@@ -2,3 +2,4 @@
 da comissão do distribuidor e dos impostos. A comissão e os impostos são calculados 
 sobre o custo de fábrica, conforme a tabela abaixo. Leia o custo de fábrica 
 e escreva o custo ao consumidor.'''
+
--- revisao2/ex8.py
+++ revisao2/ex8.py
@@ -2,3 +2,14 @@
 um carro em um percurso, calcule o consumo em km/l e escreva uma mensagem de acordo 
 com a tabela abaixo:'''
 
+km = float(input("Qual a distancia em km: "))
+gasolina = float(input("Qual a quantidade de gasolina: "))
+
+calculo = km / gasolina 
+
+if calculo < 8:
+    print("Venda o carro!")
+elif calculo >= 8 and calculo <= 12:
+    print("Econômico")
+else:
+    print("Super econômico")
